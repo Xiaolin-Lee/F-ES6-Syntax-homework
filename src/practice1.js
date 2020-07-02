f309293c070433652ceb0c3bdaab06f89adb6c82
Xiaolin-Lee <xlli@thoughtworks.com>
2020-07-03 00:56:08 +0800

    [xiaolin] fix tesst use array function

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,15 @@
-const parseData
+const parseData = (inputData) => {
+    const {column: keys, data} = inputData;
+    return data.map(dataItem => parseResultItem(dataItem, keys));
+};
+
+const parseResultItem = (dataItem, keys) => {
+    const resultItem = {};
+    for (let [index, value] of dataItem.entries()) {
+        const {name: key} = keys[index];
+        resultItem[key] = value;
+    }
+    return resultItem;
+};
+
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,10 @@
-const inject
+const inject = (items, sections) => {
+    const result = items.slice();
+    sections = sections.sort((item, next) => next.index - item.index);
+
+    for (let {content, index} of sections) {
+        result.splice(index, 0, content);
+    }
+    return result;
+};
 export { inject };
